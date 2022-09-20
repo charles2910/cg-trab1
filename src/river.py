784f8fff0a151a3199c15d30ee7e38c6976afac5
Maíra Canal <mairacanal@riseup.net>
2022-09-20 10:40:53 -0300

    src: add River documentation

--- src/river.py
+++ src/river.py
@@ -9,10 +9,21 @@ from object import Object, Color
 import numpy as np
 
 class River(Object):
+    """
+        A class used to represent the river. Child of class Object.
+
+        ...
+
+        Attributes
+        ----------
+        program : class 'ctypes.c_uint'
+        an object to which the shader objects will be attached
+    """
     def __init__(self, program):
         super().__init__(program, Color(0.255, 0.412, 0.882))
 
     def create(self):
+        '''Define the vertex of the river'''
         vertices = np.zeros(4, [("position", np.float32, 2)])
         vertices['position'] = [
             (-1.0, -1.0),
