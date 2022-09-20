c7084573cfa00961c6bc000d463c551b26a07dbc
Maíra Canal <mairacanal@riseup.net>
2022-09-20 10:43:36 -0300

    src: add Sun documentation

--- src/sun.py
+++ src/sun.py
@@ -8,5 +8,16 @@ from circle import Circle, Coordinates
 from object import Color
 
 class Sun(Circle):
+    """
+        A class used to represent the sun. Child of class Circle.
+
+        ...
+
+        Attributes
+        ----------
+        program : class 'ctypes.c_uint'
+        an object to which the shader objects will be attached
+    """
     def __init__(self, program):
+        # Defines the coordinates, radius and color of the sun
         super().__init__(program, Coordinates(-0.25, 0.75), 0.18, Color(1.0, 0.843, 0))
