7a9b1d6774b96edf4573a7e0c25e066d3b1a35d0
Maíra Canal <mairacanal@riseup.net>
2022-09-20 09:31:59 -0300

    src: add Transform documentation

--- src/transform.py
+++ src/transform.py
@@ -7,10 +7,12 @@
 import numpy as np
 
 class Transform:
+    '''A class used to group all the transformation matrices'''
     def __init__(self):
         pass
 
     def translate(self, t_x, t_y):
+        '''Returns a translation matrix with offset (t_x, t_y)'''
         return np.array([
             1.0, 0.0, 0.0, t_x,
             0.0, 1.0, 0.0, t_y,
@@ -19,6 +21,7 @@ class Transform:
         np.float32)
 
     def scale(self, s_x, s_y):
+        '''Returns a scaling matrix with factor (s_x, s_y)'''
         return np.array([
             s_x, 0.0, 0.0, 0.0,
             0.0, s_y, 0.0, 0.0,
@@ -27,6 +30,7 @@ class Transform:
         np.float32)
 
     def rotate(self, ang):
+        '''Returns a rotation matrix with angle ang'''
         return np.array([
             np.cos(ang), -np.sin(ang), 0.0, 0.0,
             np.sin(ang),  np.cos(ang), 0.0, 0.0,
