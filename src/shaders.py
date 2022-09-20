ca98e1f81adf97e1dfe4d43a600f7e6cff77f014
Maíra Canal <mairacanal@riseup.net>
2022-09-20 11:04:37 -0300

    src: add Shaders documentation

--- src/shaders.py
+++ src/shaders.py
@@ -8,14 +8,30 @@ from OpenGL.GL import *
 import OpenGL.GL.shaders
 
 class Shader:
+    """
+        A class used to process the vertex and fragment shaders.
+
+        ...
+
+        Attributes
+        ----------
+        vertex_code : str
+        GLSL vertex shader code
+
+        fragment_code : str
+        GLSL fragment shader code
+    """
     def __init__(self, vertex_code: str, fragment_code: str):
+        # Request a program and shader slots from GPU
         self.program = glCreateProgram()
         vertex = glCreateShader(GL_VERTEX_SHADER)
         fragment = glCreateShader(GL_FRAGMENT_SHADER)
 
+        # Set shaders source
         glShaderSource(vertex, vertex_code)
         glShaderSource(fragment, fragment_code)
 
+        # Compile shaders
         glCompileShader(vertex)
         if not glGetShaderiv(vertex, GL_COMPILE_STATUS):
             error = glGetShaderInfoLog(vertex).decode()
@@ -26,11 +42,14 @@ class Shader:
             error = glGetShaderInfoLog(fragment).decode()
             raise RuntimeError(error)
 
+        # Attach shader objects to the program
         glAttachShader(self.program, vertex)
         glAttachShader(self.program, fragment)
 
+        # Build program
         glLinkProgram(self.program)
         if not glGetProgramiv(self.program, GL_LINK_STATUS):
             raise RuntimeError(glGetProgramInfoLog(self.program))
 
+        # Make program the default program
         glUseProgram(self.program)
