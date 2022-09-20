3121e778b70a078e322e57ca812897c208fce93b
Maíra Canal <mairacanal@riseup.net>
2022-09-20 09:34:41 -0300

    src: add Field documentation

--- src/field.py
+++ src/field.py
@@ -9,10 +9,21 @@ from object import Object, Color
 import numpy as np
 
 class Field(Object):
+    """
+        A class used to represent the field. Child of class Object.
+
+        ...
+
+        Attributes
+        ----------
+        program : class 'ctypes.c_uint'
+        an object to which the shader objects will be attached
+    """
     def __init__(self, program):
         super().__init__(program, Color(0.420, 0.557, 0.137))
 
     def create(self):
+        '''Define the vertex of the field'''
         vertices = np.zeros(4, [("position", np.float32, 2)])
         vertices['position'] = [
             (-1.0, 0.2),
