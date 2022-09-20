4c6170eb6d451a1ea5718cd513b377d2d4a1c4e5
Maíra Canal <mairacanal@riseup.net>
2022-09-20 09:10:13 -0300

    src: add Boat documentation

--- src/boat.py
+++ src/boat.py
@@ -10,24 +10,35 @@ from OpenGL.GL import *
 import numpy as np
 
 class Boat(Object):
+    """
+        A class used to represent the boat. Child of Object.
+
+        ...
+
+        Attributes
+        ----------
+        program : class 'ctypes.c_uint'
+        an object to which the shader objects will be attached
+    """
     def __init__(self, program):
         super().__init__(program, None)
 
     def create(self):
+        '''Define the vertex of the boat'''
         vertices = np.zeros(11, [("position", np.float32, 2)])
         vertices['position'] = [
-            # Casco
+            # Hull
             (-0.78, -0.89),
             (-0.84, -0.76),
             (-0.40, -0.76),
             (-0.46, -0.89),
 
-            # Vela
+            # Sail
             (-0.62, -0.75),
             (-0.88, -0.75),
             (-0.62, -0.56),
 
-            # Mastro
+            # Mast
             (-0.62, -0.76),
             (-0.62, -0.56),
             (-0.58, -0.76),
@@ -36,12 +47,18 @@ class Boat(Object):
         return vertices
 
     def draw(self):
+        '''Draw the boat in the window with the proper colors'''
         glBindVertexArray(self.vao)
         glUniformMatrix4fv(glGetUniformLocation(self.program, "mat_transformation"), 1, GL_TRUE, self.mat_transformation)
+
+        # Color and draw the hull
         glUniform4f(glGetUniformLocation(self.program, "color"), 0.0, 0.0, 0.5, 1.0)
         glDrawArrays(GL_QUADS, 0, 4)
+        # Color and draw the sail
         glUniform4f(glGetUniformLocation(self.program, "color"), 0.7, 0.2, 0.2, 1.0)
         glDrawArrays(GL_TRIANGLE_STRIP, 4, 3)
+        # Color and draw the mast
         glUniform4f(glGetUniformLocation(self.program, "color"), 0.2, 0.3, 0.3, 1.0)
         glDrawArrays(GL_TRIANGLE_STRIP, 7, 4)
+
         glBindVertexArray(0)
